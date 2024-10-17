bb8a485ee50077be3e6748e8ee8acaf08eb49cfd
MaTThewB0rg <msborg858@gmail.com>
2024-10-17 15:10:06 -0700

    Update GameLevelWater.js

--- assets/js/rpg2x/GameLevelWater.js
+++ assets/js/rpg2x/GameLevelWater.js
@@ -27,7 +27,7 @@ class GameLevelWater {
 
     // Player 1 sprite data (turtle)
     const TURTLE_SCALE_FACTOR = 10;
-    const sprite_src_turtle = path + "/images/rpg/manwalkvery.png";
+    const sprite_src_turtle = path + "/images/rpg/rightnowman.png";
     const sprite_data_turtle = {
         name: 'turtle',
         src: sprite_src_turtle,
