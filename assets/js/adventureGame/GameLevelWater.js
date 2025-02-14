0759861f4b7c2467103b9ac17d7cc33ef9e3fd34
MaTThewB0rg <mborg858@gmail.com>
2025-02-14 13:36:36 -0800

    Change octopus

--- assets/js/adventureGame/GameLevelWater.js
+++ assets/js/adventureGame/GameLevelWater.js
@@ -21,22 +21,22 @@ class GameLevelWater {
         pixels: {height: 168, width: 300}
     };
 
-    const sprite_src_octopus = path + "/images/gamify/octopus.png"; // be sure to include the path
+    const sprite_src_octopus = path + "/images/gamify/rightnowman.png"; // be sure to include the path
     const OCTOPUS_SCALE_FACTOR = 5;
     const sprite_data_octopus = {
         id: 'Octopus',
-        greeting: "Hi I am Octopus, the water wanderer. I am looking for wisdome and adventure!",
+        greeting: "Hi I am Liam, the lone operator no one knows about. I am looking for wisdome and adventure!",
         src: sprite_src_octopus,
         SCALE_FACTOR: OCTOPUS_SCALE_FACTOR,
         STEP_FACTOR: 1000,
         ANIMATION_RATE: 50,
         INIT_POSITION: { x: 0, y: height - (height/OCTOPUS_SCALE_FACTOR) }, 
-        pixels: {height: 250, width: 167},
-        orientation: {rows: 3, columns: 2 },
-        down: {row: 0, start: 0, columns: 2 },
-        left: {row: 1, start: 0, columns: 2 },
-        right: {row: 1, start: 0, columns: 2 },
-        up: {row: 0, start: 0, columns: 2},
+        pixels: {height: 183, width: 275},
+        orientation: {rows: 4, columns: 6 },
+        down: {row: 0, start: 0, columns: 6 },
+        left: {row: 1, start: 0, columns: 6 },
+        right: {row: 2, start: 0, columns: 6 },
+        up: {row: 3, start: 0, columns: 6},
         hitbox: { widthPercentage: 0.45, heightPercentage: 0.2 },
         keypress: { up: 87, left: 65, down: 83, right: 68 } // W, A, S, D
     };
